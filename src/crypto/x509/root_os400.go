db34a512cb764afbc563e5015794b31d47e5b54f
Kevin Adler <kadler@us.ibm.com>
2020-10-29 00:14:14 -0500

    WIP: Copy AIX files to OS400
    For now we just copy as-is (other than the mmap region changes), but
    more changes are necessary to adjust for PASE's deficiencies in API
    support, eg. *at functions, clock_getres/clock_gettime, etc.

---
+++ src/crypto/x509/root_os400.go
@@ -0,0 +1,11 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package x509
+
+// Possible certificate files; stop after finding one.
+var certFiles = []string{
+	// TODO(os400): We don't ship a bundle
+	//"/QOpenSys/var/lib/ca-certificates/ca-bundle.crt",
+}
